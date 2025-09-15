167ca45675d70a26de1fe7d604eec3988c35ce89
Fernanda Carvalho de Proença Braido <152922167+Fefecarvalho@users.noreply.github.com>
2025-09-15 07:59:52 -0300

    15/09-2

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function mostraPalvrasChave () {
 
     campoResultado.textContent = palavrasChave.join(",");
 }
+
+function processaTexto(texto){
+    let palavras = TransformStreamDefaultController.split(/\P{L}+/u);
+
+    for (let i in palavras){
+        palavras [i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencia(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1,p2){
+        return frequencias[p2] - frequencias[p1];
+    }
+
+}
